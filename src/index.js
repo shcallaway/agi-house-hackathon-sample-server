9e8faede436d05b54cd148ab138f0d8b26d14055
Sherwood Callaway <shcallaway@gmail.com>
2025-04-19 18:14:39 -0700

    asdf

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 // Define routes
 router.get("/", (req, res) => {
+  logger.info("Received request to /");
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end(
     "Hello! I am a sample server implemented for the AGI House hackathon.\n"
--- src/logger.js
+++ src/logger.js
@@ -3,6 +3,8 @@ import DatadogWinston from "datadog-winston";
 
 const DD_API_KEY = process.env.DD_API_KEY;
 const DD_HOSTNAME = process.env.DD_HOSTNAME;
+const DD_SERVICE =
+  process.env.DD_SERVICE || "agi-house-hackathon-sample-server";
 
 class Logger {
   constructor() {
@@ -10,11 +12,12 @@ class Logger {
 
     // If Datadog API key and hostname are set, configure Winston with Datadog transport
     if (DD_API_KEY && DD_HOSTNAME) {
+      this.log("Configuring Datadog log transport");
       this.w.add(
         new DatadogWinston({
           apiKey: DD_API_KEY,
           hostname: DD_HOSTNAME,
-          service: "agi-house-hackathon-sample-server",
+          service: DD_SERVICE,
           ddsource: "nodejs",
         })
       );
